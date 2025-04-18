73a6cba97e33d9dda8cc9e5ddbe65cdaf3317440
Juandomz91 <juandomz87@gmail.com>
2025-04-18 07:39:02 +0000

    pedal

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,46 @@
 import React from "react";
+import { NavBar, Jumbotron, Card, Footer } from '../main.jsx';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <NavBar />
+      <div className="container mt-4">
+        <Jumbotron />
+        <div className="row mt-4">
+          <div className="col-md-3">
+            <Card 
+              title="Surf" 
+              text="& tequila!!" 
+              image="https://imgs.search.brave.com/8Lr8nsBB6Vgp6k0lQKYJvfxmt7MaQDBaNBJWzN3VNRA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjg5/NzI1NTY2L3Bob3Rv/L3RlcXVpbGEtc2hv/dHMtd2l0aC1zYWx0/LWFuZC1saW1lLmpw/Zz9zPTYxMng2MTIm/dz0wJms9MjAmYz1Y/M1MwWkJNSHBWY0dN/VjBqUTdXMldYR1FM/amtWNDBOajgtallO/dTNua2NzPQ" 
+            />
+          </div>
+          <div className="col-md-3">
+            <Card 
+              title="Furgoneteo..." 
+              text="& TEQUILA!!!" 
+              image="https://imgs.search.brave.com/1dmOZqtgBTi_6LqZbTYyu26QjtqH4YaNw7krDk6JCQ8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly95ZXNj/YXBhLnR3aWMucGlj/cy9saWJyYXJ5L21l/ZGlhL3BpY3R1cmVz/L2Zyb250ZW5kL3Vz/ZXJzLWFuZC12YW4u/anBnP3R3aWM9djE" 
+            />
+          </div>
+          <div className="col-md-3">
+		  <Card 
+              title="Un gatete" 
+              text="&....TEQUILA!!" 
+              image="https://imgs.search.brave.com/PM1__kcgvA7cfoyYXGndv6HtQ1pUfniiAoo6kB-iVb4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLmlt/Z2ZsaXAuY29tLzIv/c2U0anguanBn" 
+            />
+          </div>
+          <div className="col-md-3">
+            <Card 
+              title="Tequila" 
+              text="& THE KILLAH!!" 
+              image="https://imgs.search.brave.com/zAyvgCHMyVd5ZO494TsR-vYZFSNsQ2SgyFM6bvKGazg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9zb21i/cmVyby1tZXhpY2Fu/by1kZWwtcGlzdG9s/ZXJvLWRlbC1iaWdv/dGUtZGVsLXJldiVD/MyVCM2x2ZXItZGVs/LWJhbmRpZG8tMTk1/NzA0OTguanBn" 
+            />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -12,22 +12,22 @@ import '../styles/index.css'
 import Home from './components/Home';
 
 function NavBar(){
-  return <nav className="navbar navbar-expand-lg bg-light">
+  return <nav className=" navbar navbar-expand-lg  bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand text-light" href="#">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
+      <ul className="navbar-nav ms-auto">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active text-light" aria-current="page" href="#">Borrachera</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <a className="nav-link text-light" href="#">Cogorza</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <a className="nav-link text-light" href="#">Melopea</a>
         </li>
        
       </ul>
@@ -37,27 +37,41 @@ function NavBar(){
 };
 
 function Jumbotron() {
-  return <div class="jumbotron">
-  <h1 className="display-4">Hello, world!</h1>
-  <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  return <div className="jumbotron">
+  <h1 className="display-4">Ey que pasa?!</h1>
+  <p className="lead">Desde los que aprobaron recreo pero suspendieron en proyecto hombre llega... </p>
   
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p>CH3-CH2OH</p>
   <p className="lead">
     <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
   </p>
 </div>
 };
 
-function Card () {
-   <div className="card" style="width: 18rem;">
-   <img src="..." class="card-img-top" alt="..."/>
-   <div className="card-body">
-     <h5 className="card-title">Card title</h5>
-     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-     <a href="#" className="btn btn-primary">Go somewhere</a>
-   </div>
- </div>
-};
+function Card({ title, text, image }) {
+  return (
+    <div className="card" style={{ width: '18rem' }}>
+      <img src={image} className="card-img-top" alt={title} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{text}</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-center text-light py-3 mt-4">
+      <p>No estoy loco, mi mamá me hizo pruebas</p>
+    </footer>
+  );
+}
+
+
+export { NavBar, Jumbotron, Card, Footer };
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
